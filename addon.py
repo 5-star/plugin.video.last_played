69bed19445f9d9c2ebb2f359e4733096b090839f
Luis Rodrigues <luis@rodrigues.pt>
2016-07-16 09:57:04 +0100

    Improve metadata. Allow Item removal

--- addon.py
+++ addon.py
@@ -16,44 +16,8 @@ menu = args.get('menu', None)
 list_size = int(addon.getSetting('list_size'))
 show_addons = addon.getSetting('show_addons')
 lang = addon.getLocalizedString
+profile = xbmc.translatePath(addon.getAddonInfo('profile')).decode("utf-8")
 	
-def openDB():	
-	dbType = 'sqlite'	
-	dbName = 'MyVideos'
-	kodiVer = xbmc.getInfoLabel( "System.BuildVersion" )[:2]
-	dbVersion = '99'
-	if kodiVer==13: dbVersion = '75'
-	if kodiVer==14: dbVersion = '90'
-	if kodiVer==15: dbVersion = '93'
-	advPath = xbmc.translatePath(os.path.join('special://profile','advancedsettings.xml'))
-	if xbmcvfs.exists(advPath):
-		f = open(advPath, "r")
-		advSettings = f.read()
-		wrk = re.compile('<videodatabase>(.+?)</videodatabase>', re.DOTALL).findall(advSettings)
-		if len(wrk) != 0:
-			dbSection = wrk[0]
-		wrk = re.compile('<type>(.+?)</type>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbType = wrk[0]
-		wrk = re.compile('<name>(.+?)</name>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbName = wrk[0]
-		wrk = re.compile('<host>(.+?)</host>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbHost = wrk[0]
-		wrk = re.compile('<port>(.+?)</port>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbPort = wrk[0]
-		wrk = re.compile('<user>(.+?)</user>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbUser = wrk[0]
-		wrk = re.compile('<pass>(.+?)</pass>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbPass = wrk[0]
-	if dbType=='mysql':
-		import mysql.connector as database
-		db = database.connect(db = dbName+dbVersion, user = dbUser, passwd = dbPass, host = dbHost)
-	else:
-		from sqlite3 import dbapi2 as database
-		dbPath = xbmc.translatePath('special://database/' + dbName + dbVersion + '.db')
-		db = database.connect(dbPath)
-		db.row_factory = database.Row
-	return db
-
 def url(pQuery):
 	return sys.argv[0] + '?' + urllib.urlencode(pQuery)
 
@@ -63,14 +27,9 @@ def jsonrpc(query):
 def list_items(items, type):
 	xbmcplugin.setContent(addon_handle, type)
 	for item in items:
-		if type=='movies': dbid = item['movieid']
-		elif type=='episodes': dbid = item['episodeid']
-		elif type=='musicvideos': dbid = item['musicvideoid']
-		elif type=='songs': dbid = item['songid']
 		li = ListItem(label=item['title'])
 		li.setInfo(type="Video", infoLabels={"mediatype": 'episode'})
 		li.setProperty("type", "episode")
-		li.setProperty("dbid", str(dbid))
 		addDirectoryItem(addon_handle, item['file'], li, False, len(items))
 	endOfDirectory(addon_handle)
 	
@@ -80,21 +39,26 @@ def get_items(pMethod, pResult):
 	return items
 	
 def list_others():
-	try:
-		db = openDB()
-		cursor = db.cursor()
-		cursor.execute('SELECT strFilename, strpath, files.dateadded, lastplayed FROM files INNER JOIN path on files.idpath=path.idpath where strhash is null Order By lastPlayed desc LIMIT ' + str(list_size))
-		for row in cursor.fetchall():
-			title = row[2]
-			if title=='' or title=='None' or title is None: title=row[3]
-			li = ListItem(label=title)
-			li.setInfo(type="Video", infoLabels={ "Title": title})
-			li.setProperty('IsPlayable', 'true')
-			addDirectoryItem(addon_handle, row[1]+row[0], li, False)
+	xbmcplugin.setContent(addon_handle, "movies")
+	txtfile = profile + "list.txt"
+	if xbmcvfs.exists(txtfile):
+		with open(txtfile) as f:
+			lines = f.readlines()
+		nbr=0
+		for line in lines:
+			item = line.split(chr(9))
+			if len(item)>2 and nbr<list_size:
+				li = ListItem(label=item[0])
+				li.setInfo(type="Video", infoLabels={ "Title": item[0]})
+				li.setProperty('IsPlayable', 'true')
+				command = []
+				command.append((lang(30008), "XBMC.RunPlugin(plugin://plugin.video.last_played?menu=remove&id="+str(nbr)+")"))
+				nbr=nbr+1
+				li.addContextMenuItems(command)
+				li.setArt({ "poster" : item[2].strip() })
+				addDirectoryItem(addon_handle, item[1].strip(), li, False)
 		endOfDirectory(addon_handle)
-	except: 
-		pass
-
+		
 if menu is None:
 	xbmcplugin.setContent(addon_handle, "menu")
 	imgPath=xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('path')).decode('utf-8')
@@ -103,7 +67,7 @@ if menu is None:
 	addDirectoryItem(addon_handle, url({'menu': 'musicvideos'}), ListItem(lang(30004), iconImage=imgPath+'/resources/musicvideos.png'), True)
 	addDirectoryItem(addon_handle, url({'menu': 'songs'}), ListItem(lang(30005), iconImage=imgPath+'/resources/songs.png'), True)
 	if show_addons == "true":
-		addDirectoryItem(addon_handle, url({'menu': 'others'}), ListItem(lang(30006), iconImage=imgPath+'/resources/others.png'), True)
+		addDirectoryItem(addon_handle, url({'menu': 'addons'}), ListItem(lang(30006), iconImage=imgPath+'/resources/others.png'), True)
 	endOfDirectory(addon_handle)
 elif menu[0] == 'movies':
 	list_items(get_items("VideoLibrary.GetMovies","movies"),'movies')
@@ -113,5 +77,20 @@ elif menu[0] == 'musicvideos':
 	list_items(get_items("VideoLibrary.GetMusicVideos","musicvideos"),"musicvideos")
 elif menu[0] == 'songs':
 	list_items(get_items("AudioLibrary.GetSongs","songs"),"songs")
+elif menu[0] == 'remove':
+	lid = args.get('id', None)
+	txtfile = profile + "list.txt"
+	if xbmcvfs.exists(txtfile) and lid is not None:
+		rid = int(lid[0])
+		with open(txtfile) as f:
+			lines = f.readlines()
+			nbr=0
+			with open(txtfile, 'w') as f:
+				for line in lines:
+					item = line.split(chr(9))
+					if len(item)>2 and nbr!=rid:
+						f.write(line)
+					nbr=nbr+1
+		xbmc.executebuiltin("ActivateWindow(Videos,plugin://plugin.video.last_played?menu=addons)")
 else:
 	list_others()
--- default.py
+++ default.py
@@ -1,82 +1,91 @@
 import os, re, sys, time
+import json
+import urllib
 import xbmc
+import xbmcgui
 import xbmcaddon
 import xbmcvfs
 
-__addon__ = xbmcaddon.Addon()
-__profile__ = xbmc.translatePath( __addon__.getAddonInfo('profile') ).decode("utf-8")
-getTitle=''
+addon = xbmcaddon.Addon()
+profile = xbmc.translatePath(addon.getAddonInfo('profile')).decode("utf-8")
+list_size = int(addon.getSetting('list_size'))
 
-def openDB():	
-	dbType = 'sqlite'	
-	dbName = 'MyVideos'
-	kodiVer = xbmc.getInfoLabel( "System.BuildVersion" )[:2]
-	dbVersion = '99'
-	if kodiVer==13: dbVersion = '75'
-	if kodiVer==14: dbVersion = '90'
-	if kodiVer==15: dbVersion = '93'
-	advPath = xbmc.translatePath(os.path.join('special://profile','advancedsettings.xml'))
-	if xbmcvfs.exists(advPath):
-		f = open(advPath, "r")
-		advSettings = f.read()
-		wrk = re.compile('<videodatabase>(.+?)</videodatabase>', re.DOTALL).findall(advSettings)
-		if len(wrk) != 0:
-			dbSection = wrk[0]
-		wrk = re.compile('<type>(.+?)</type>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbType = wrk[0]
-		wrk = re.compile('<name>(.+?)</name>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbName = wrk[0]
-		wrk = re.compile('<host>(.+?)</host>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbHost = wrk[0]
-		wrk = re.compile('<port>(.+?)</port>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbPort = wrk[0]
-		wrk = re.compile('<user>(.+?)</user>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbUser = wrk[0]
-		wrk = re.compile('<pass>(.+?)</pass>', re.DOTALL).findall(dbSection)
-		if len(wrk) != 0: dbPass = wrk[0]
-	if dbType=='mysql':
-		import mysql.connector as database
-		db = database.connect(db = dbName+dbVersion, user = dbUser, passwd = dbPass, host = dbHost)
-	else:
-		from sqlite3 import dbapi2 as database
-		dbPath = xbmc.translatePath('special://database/' + dbName + dbVersion + '.db')
-		db = database.connect(dbPath)
-		db.row_factory = database.Row
-	return db
-
-class AutoSubsPlayer(xbmc.Player):
+class KodiPlayer(xbmc.Player):
 	def __init__(self, *args, **kwargs):
 		xbmc.Player.__init__(self)
 
-	def onPlayBackStarted(self):
-		global getTitle
-		if xbmc.Player().isPlayingVideo():
-			vTag = xbmc.Player().getVideoInfoTag()
-			getTitle = vTag.getTitle().strip()
-			if getTitle != '':
-				getYear = vTag.getYear()
-				if getYear > 0: getTitle = getTitle + " (" + str(getYear) + ")"
-		else: getTitle = "NOTVIDEO"
-				
-	def onPlayBackStopped(self):
-		if getTitle != "NOTVIDEO":
-			xbmc.log("------getTitle '%s'" % getTitle, 3)
-			time.sleep(0.5)
-			try:
-				db = openDB()
-				cursor = db.cursor()
-				cursor.execute('SELECT idfile, strhash, files.dateadded FROM files INNER JOIN path on files.idpath=path.idpath Order By lastPlayed desc LIMIT 1')
-				for row in cursor.fetchall():
-					if row[1] is None:
-						xbmc.log("-----------id '%s'" % row[0], 3)
-						sql = "UPDATE files SET dateadded='%s' where idfile=%s" % (getTitle, row[0])
-						cursor = db.cursor()
-						cursor.execute(sql)
-						db.commit()
-			except:
-				pass
+	# Checks JSON response and returns boolean result
+	def _checkReponse(self, response):
+		result = False
+		if ( ('result' in response) and ('error' not in response) ):
+			result = True
+		return result
+		
+	# Executes JSON request and returns the JSON response
+	def _execute(self, request):
+		request_string = json.dumps(request)
+		response = xbmc.executeJSONRPC(request_string)  
+		if ( response ):
+			response = json.loads(response)
+		return response
+    
+	# Builds JSON request with provided json data
+	def _buildRequest(self, method, params={}, jsonrpc='2.0', rid='1'):
+		request = { 'jsonrpc' : jsonrpc, 'method' : method, 'params' : params, 'id' : rid, }
+		return request
+		
+	# Performs single JSON query and returns result boolean, data dictionary and error string
+	def _query(self, request):
+		result = False
+		data = {}
+		error = ''
+		if ( request ):
+			response = self._execute(request)
+			if ( response ):
+				result = self._checkReponse(response)
+				if ( result ):
+					data = response['result']
+				else: error = response['error']
+		return (result, data, error)
 
-player_monitor = AutoSubsPlayer()
+	def onPlayBackStarted(self):
+		request = self._buildRequest('Player.GetItem', {'playerid' : 1, 'properties' : ['file', 'title', 'year', 'thumbnail', 'fanart']})
+		result, data = self._query(request)[:2]
+		if ( result and 'item' in data and data['item'] ):
+			item = data['item']
+			pu=xbmc.Player().getVideoInfoTag().getPictureURL()
+			if pu=='' and item['type']!='musicvideo':
+				xbmc.log("#############item '%s'" % str(item), 3)
+				getTitle=xbmc.Player().getVideoInfoTag().getTitle().strip()
+				if getTitle=='': getTitle=item['title']
+				if getTitle=='': getTitle=item['label']
+				if getTitle != '':
+					getYear = item['year']
+					if getYear > 0: getTitle = getTitle + " (" + str(getYear) + ")"
+					txtfile = profile + "list.txt"
+				if item['thumbnail']=='': img = item['fanart'].strip()
+				else: img = item['thumbnail'].strip()
+				img = img.rstrip('/').replace('image://','')
+				img = urllib.unquote(img)
+				if not os.path.exists(profile):
+					os.makedirs(profile)
+				if xbmcvfs.exists(txtfile):
+					with open(txtfile) as f:
+						lines = f.readlines()
+				else: lines = {}
+				with open(txtfile, 'w') as f:
+					url=item['file'].strip()
+					list_count=1
+					f.write(getTitle+chr(9)+url+chr(9)+img+chr(10))
+					for line in lines:
+						lin = line.split(chr(9))
+						if len(lin)>2 and list_count<(list_size+20):
+							#xbmc.log("#############item[file] '%s'" % item['file'].strip(), 3)
+							if lin[1]!=url:
+								f.write(line)
+								list_count=list_count+1
+
+player_monitor = KodiPlayer()
 
 while not xbmc.abortRequested:
 	xbmc.sleep(1000)
